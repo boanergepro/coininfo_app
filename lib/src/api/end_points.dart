79440b7fd97827bae564a971c6ce1a17ae06f0a9
boanergepro <antonycarrizo96@gmail.com>
2019-07-10 12:03:53 -0400

    [MOD]: - Agregue funcion y end point para manejar los iconos.

--- lib/src/api/end_points.dart
+++ lib/src/api/end_points.dart
@@ -1,1 +1,2 @@
 const String ALL_CURRENCIES = 'https://api.coincap.io/v2/assets?limit=5';
+const String ICON_CURRENCY = 'https://static.coincap.io/assets/icons/xrp'
---
+++ lib/src/utils/parce_url_icon.dart
@@ -0,0 +1,5 @@
+import 'package:coininfo/src/api/end_points.dart';
+
+String parceUrlIcon(String nameCoin) {
+  return ICON_CURRENCY + nameCoin + '@2x.png';
+}
